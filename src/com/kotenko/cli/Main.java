71194e2edc5b25eace6332266df63e97debefecb
Alex <akotenko@prezna.com>
2023-11-14 19:13:34 -0500

    initial commit

---
+++ src/com/kotenko/cli/Main.java
@@ -0,0 +1,13 @@
+package com.kotenko.cli;
+
+public class Main {
+    public static void main(String[] args) {
+        //1️⃣ - Book Car
+        //2️⃣ - View All User Booked Cars
+        //3️⃣ - View All Bookings
+        //4️⃣ - View Available Cars
+        //5️⃣ - View Available Electric Cars
+        //6️⃣ - View all users
+        //7️⃣ - Exit
+    }
+}
