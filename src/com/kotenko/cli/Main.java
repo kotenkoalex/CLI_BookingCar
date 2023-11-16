49b9c2aa3e9541d0cf4ea360895f15f6c5c2b048
Alex <akotenko@prezna.com>
2023-11-15 21:41:27 -0500

    1511L_models: added Car

--- src/com/kotenko/cli/Main.java
+++ src/com/kotenko/cli/Main.java
@@ -1,13 +1,24 @@
 package com.kotenko.cli;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        //1️⃣ - Book Car
-        //2️⃣ - View All User Booked Cars
-        //3️⃣ - View All Bookings
-        //4️⃣ - View Available Cars
-        //5️⃣ - View Available Electric Cars
-        //6️⃣ - View all users
-        //7️⃣ - Exit
+        Scanner scanner = new Scanner(System.in);
+        boolean isWork = true;
+        while (scanner.hasNext()) {
+            System.out.println("""
+                    1️⃣ - Book Car
+                    2️⃣ - View All User Booked Cars
+                    3️⃣ - View All Bookings
+                    4️⃣ - View Available Cars
+                    5️⃣ - View Available Electric Cars
+                    6️⃣ - View all users
+                    7️⃣ - Exit
+                    """.trim()
+            );
+        }
+
+
     }
 }
